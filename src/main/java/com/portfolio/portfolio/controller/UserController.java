8505f86fcbcb58a1e15da061c5dafbe32d690ae3
apidemo <deepakkumarnaik3@gmail.com>
2025-07-07 23:03:49 +0530

    Initial commit

---
+++ src/main/java/com/portfolio/portfolio/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.portfolio.portfolio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @GetMapping("/get")
+    public  String  hello(){
+        return "hello world";
+    }
+}
